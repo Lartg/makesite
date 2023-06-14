26f6a4077360fef01d8937bbba2b5d9ba57f18df
Lartg <roc.gerrety@gmail.com>
2023-06-13 19:05:10 -0700

    get the whole flag deal now

--- makesite.go
+++ makesite.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readTextFlag() {
+	text := flag.String("text", "", "text to print")
+	flag.Parse()
+	fmt.Println(*text)
+}
+
+func readTextFileFlag() {
+	file := flag.String("file", "", "textfile to be read")
+	flag.Parse()
+	text, err := os.ReadFile(*file)
+	check(err)
+
+	fmt.Println(string(text))
+
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	readTextFileFlag()
 }
