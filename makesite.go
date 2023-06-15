5c470b1831263baa9117399294900f0b8f939c2b
Lartg <roc.gerrety@gmail.com>
2023-06-15 16:50:48 -0700

    the template is fixed and writing to a new html file is cool and nice

--- makesite.go
+++ makesite.go
@@ -2,32 +2,61 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"html/template"
+	"io/ioutil"
 	"os"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func readTextFlag() {
-	text := flag.String("text", "", "text to print")
-	flag.Parse()
-	fmt.Println(*text)
-}
-
-func readTextFileFlag() {
-	file := flag.String("file", "", "textfile to be read")
-	flag.Parse()
-	text, err := os.ReadFile(*file)
-	check(err)
-
-	fmt.Println(string(text))
-
+type Content struct {
+	Header    string
+	Paragraph string
 }
 
 func main() {
-	readTextFileFlag()
+	// flag to specify the file path
+	filePath := flag.String("file", "", "Path to the input text file")
+	flag.Parse()
+
+	// check if file
+	if *filePath == "" {
+		panic("Please provide a file path using the -file flag")
+	}
+
+	// read the text from file
+	text, err := ioutil.ReadFile(*filePath)
+	if err != nil {
+		panic("Error reading file should be a text file")
+	}
+
+	// create the data structure for the template
+	content := Content{
+		Header:    "Template Header",
+		Paragraph: string(text),
+	}
+
+	// read the template file
+	templateFile, err := ioutil.ReadFile("template.tmpl")
+	if err != nil {
+		panic("failed to read template")
+	}
+
+	template, err := template.New("template.tmpl").Parse(string(templateFile))
+	if err != nil {
+		panic("failed to parse template")
+	}
+	// create the output file
+	HTMLfile, err := os.Create("output.html")
+	if err != nil {
+		panic("failed to create output.html")
+	}
+
+	// write template to output.html
+	err = template.Execute(HTMLfile, content)
+	if err != nil {
+		panic("failed to write content to output.html")
+	}
+
+	// close file done using
+	HTMLfile.Close()
+
 }
