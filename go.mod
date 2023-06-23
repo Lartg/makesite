fd3e8209ad84fe32694c67bb370e2c4937cb0942
Lartg <roc.gerrety@gmail.com>
2023-06-22 18:41:48 -0700

    the big blow-up of gofuckyourself

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module makesite
-
+require github.com/JoshuaDoes/gofuckyourself v0.0.0-20181118040300-9fac3800924b
 go 1.13
--- makesite.go
+++ makesite.go
@@ -1,76 +1,117 @@
-package main
+// package main
 
-import (
-	"flag"
-	"html/template"
-	"io/ioutil"
-	"os"
-	// swearfilter "github.com/JoshuaDoes/gofuckyourself"
-)
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"html/template"
+// 	"io/ioutil"
+// 	"os"
+// 	"strings"
 
-/*
-Use the Go swearfilter module
+// 	swearfilter "github.com/JoshuaDoes/gofuckyourself"
+// )
 
-1. Import swearfilter by uncommenting.
+// /*
+// Use the Go swearfilter module
 
-2. declare swears in a flag and save as list of strings in {}
+// DONE
+// 1. Import swearfilter by uncommenting.
 
-3. use our current content var as message to be filtered
+// DONE
+// 2. declare swears in a flag and save as list of strings in {}
 
-4. filter message and return html as normal
+// DONE
+// 3. use our current content var as message to be filtered
 
-*/
+// DONE
+// 4. filter message and return html as normal
 
-type Content struct {
-	Header    string
-	Paragraph string
-}
+// If I cannot get this to work then use these:
+// "github.com/bregydoc/gtranslate"
+//  "github.com/fatih/color"
+//  translatedContent, err := gtranslate.TranslateWithParams(
+//   postContent,
+//   gtranslate.TranslationParams{
+//    From: "en",
+//    To:   "es",
+//   },
+//  )
+//  if err != nil {
+//   panic(err)
+//  }
 
-func main() {
-	// flag to specify the file path
-	filePath := flag.String("file", "", "Path to the input text file")
-	flag.Parse()
+// */
 
-	// check if file
-	if *filePath == "" {
-		panic("Please provide a file path using the -file flag")
-	}
+// type Content struct {
+// 	Header    string
+// 	Paragraph string
+// }
 
-	// read the text from file
-	text, err := ioutil.ReadFile(*filePath)
-	if err != nil {
-		panic("Error reading file should be a text file")
-	}
+// func main() {
+// 	// flag for list of swears
+// 	swears := flag.String("swears", "", "Comma-separated list of strings")
 
-	// create the data structure for the template
-	content := Content{
-		Header:    "Template Header",
-		Paragraph: string(text),
-	}
+// 	// parse the command-line flags
+// 	flag.Parse()
 
-	// read the template file
-	templateFile, err := ioutil.ReadFile("template.tmpl")
-	if err != nil {
-		panic("failed to read template")
-	}
+// 	// split into a slice of strings
+// 	swearList := strings.Split(*swears, ",")
 
-	// computer understand file
-	template, err := template.New("template.tmpl").Parse(string(templateFile))
-	if err != nil {
-		panic("failed to parse template")
-	}
-	// create the output file
-	HTMLfile, err := os.Create("output.html")
-	if err != nil {
-		panic("failed to create output.html")
-	}
+// 	// INIT swear filter
+// 	filter := swearfilter.New(false, false, false, false, false, swearList...)
 
-	// write template to output.html
-	err = template.Execute(HTMLfile, content)
-	if err != nil {
-		panic("failed to write content to output.html")
-	}
+// 	// flag to specify the file path
+// 	filePath := flag.String("file", "", "Path to the input text file")
+// 	flag.Parse()
 
-	// close file done using
-	HTMLfile.Close()
-}
+// 	// check if file
+// 	if *filePath == "" {
+// 		panic("Please provide a file path using the -file flag")
+// 	}
+
+// 	// read the text from file
+// 	text, err := ioutil.ReadFile(*filePath)
+// 	if err != nil {
+// 		panic("Error reading file should be a text file")
+// 	}
+
+// 	//----------------------------
+// 	// Filter Text for swears
+// 	swearFound, swearsFound, err := filter.Check(string(text))
+// 	fmt.Println("Swear found: ", swearFound)
+// 	fmt.Println("Swears tripped: ", swearsFound)
+// 	fmt.Println("Error: ", err)
+// 	//----------------------------
+
+// 	// create the data structure for the template
+// 	content := Content{
+// 		Header:    "Template Header",
+// 		Paragraph: string(text),
+// 	}
+
+// 	// read the template file
+// 	templateFile, err := ioutil.ReadFile("template.tmpl")
+// 	if err != nil {
+// 		panic("failed to read template")
+// 	}
+
+// 	// computer understand file
+// 	template, err := template.New("template.tmpl").Parse(string(templateFile))
+// 	if err != nil {
+// 		panic("failed to parse template")
+// 	}
+// 	// create the output file
+// 	HTMLfile, err := os.Create("output.html")
+// 	if err != nil {
+// 		panic("failed to create output.html")
+// 	}
+
+// 	// write template to output.html
+// 	err = template.Execute(HTMLfile, content)
+// 	if err != nil {
+// 		panic("failed to write content to output.html")
+// 	}
+
+// 	// close file done using
+// 	HTMLfile.Close()
+// }
