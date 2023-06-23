edd22afce8e33e6b202cf4b1c11b8278f6a39be3
Lartg <roc.gerrety@gmail.com>
2023-06-22 20:25:30 -0700

    finished catching up on v 1.1

--- makesite.go
+++ makesite.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bregydoc/gtranslate"
 )
-
-/*
-If I cannot get this to work then use these:
-
- "github.com/fatih/color"
-
-
-*/
 
 type Content struct {
 	ParagraphEN string
@@ -23,62 +18,81 @@ type Content struct {
 }
 
 func main() {
-
-	// flag to specify the file path
-	filePath := flag.String("file", "", "Path to the input text file")
+	// flag for directory, default current dir
+	dir := flag.String("dir", ".", "directory path")
 	flag.Parse()
 
-	// check if file
-	if *filePath == "" {
-		panic("Please provide a file path using the -file flag")
-	}
-
-	// read the text from file
-	text, err := ioutil.ReadFile(*filePath)
-	if err != nil {
-		panic("Error reading file should be a text file")
-	}
-
-	// translate text to Japanese
-	translatedContent, err := gtranslate.TranslateWithParams(
-		string(text),
-		gtranslate.TranslationParams{
-			From: "en",
-			To:   "ja",
-		},
-	)
+	// get absolute path of the directory
+	absDir, err := filepath.Abs(*dir)
 	if err != nil {
 		panic(err)
 	}
-	// create the data structure for the template
-	content := Content{
-		ParagraphEN: string(text),
-		ParagraphJA: translatedContent,
+
+	// get all files
+	files, err := ioutil.ReadDir(absDir)
+	if err != nil {
+		panic(err)
 	}
 
-	// read the template file
-	templateFile, err := ioutil.ReadFile("template.tmpl")
-	if err != nil {
-		panic("failed to read template")
-	}
+	// find text files
+	for _, file := range files {
+		// read the text from file
+		if filepath.Ext(file.Name()) == ".txt" {
+			filePath := filepath.Join(absDir, file.Name())
+			textByte, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				fmt.Printf("Error reading file %s: %v\n", filePath, err)
+				continue
+			}
+			text := string(textByte)
 
-	// computer understand file
-	template, err := template.New("template.tmpl").Parse(string(templateFile))
-	if err != nil {
-		panic("failed to parse template")
-	}
-	// create the output file
-	HTMLfile, err := os.Create("output.html")
-	if err != nil {
-		panic("failed to create output.html")
-	}
+			// translate text to Japanese
+			translatedContent, err := gtranslate.TranslateWithParams(
+				string(text),
+				gtranslate.TranslationParams{
+					From: "en",
+					To:   "ja",
+				},
+			)
+			if err != nil {
+				panic(err)
+			}
+			// create the data structure for the template
+			content := Content{
+				ParagraphEN: string(text),
+				ParagraphJA: translatedContent,
+			}
 
-	// write template to output.html
-	err = template.Execute(HTMLfile, content)
-	if err != nil {
-		panic("failed to write content to output.html")
-	}
+			// read the template file
+			templateFile, err := ioutil.ReadFile("template.tmpl")
+			if err != nil {
+				panic("failed to read template")
+			}
 
-	// close file done using
-	HTMLfile.Close()
+			// computer understand file
+			template, err := template.New("template.tmpl").Parse(string(templateFile))
+			if err != nil {
+				panic("failed to parse template")
+			}
+
+			// make unique new file name
+			splitTextFileName := strings.Split(file.Name(), ".")
+			formattedTextFileName := splitTextFileName[0]
+			newFileName := fmt.Sprintf("%s.html", formattedTextFileName)
+			// create the output file
+			HTMLfile, err := os.Create(newFileName)
+			if err != nil {
+				panic("failed to create output.html")
+			}
+
+			// write template to output.html
+			err = template.Execute(HTMLfile, content)
+			if err != nil {
+				panic("failed to write content to output.html")
+			}
+
+			// close file done using
+			HTMLfile.Close()
+		}
+	}
 }
